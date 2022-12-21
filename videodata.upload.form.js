fd7f04eb7caa4f90af7cd5299ab5c2b530c974af
Chris Samuel <encryptable@pm.me>
2022-12-21 17:54:20 +0900

    initial commit

---
+++ videodata.upload.form.js
@@ -0,0 +1,62 @@
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_0.textContent="Lesbian";
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_0.textContent="Yes";
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_0.textContent="Professional";
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Public";
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Japanese";
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Publish Now";
+// Title
+document.getElementById("titleTmplField_0").value = "Desperate huge housewives decide become lesbian to survive the male drought";
+// Original Language
+document.getElementById("dropDownTitle_").textContent="Japanese"
+//Tags
+document.getElementById("tagsList_0").value = 
+"terashima shiho, shizuko fujiki, nozomi baba, ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+// document.getElementById('submitNewTag_0').click();
+
+// Actions
+{/* <li id="categoryId_35_0" class="categoryBtn listCount_0 alpha active">
+    <span class="categoryCheckbox"></span>
+    <span class="categoryLabel">
+        Anal    </span>
+    <div class="reset"></div>
+</li> */}
+/** Anal
+[Check]
+document.getElementById("checkbox").checked = true;
+[Uncheck]
+document.getElementById("checkbox").checked = false;
+*/
+// Final Check boxes
+// By clicking the boxes below, I certify and confirm that:
+document.getElementById("js-above18_0").checked = false;
+document.getElementById("js-verifiedContent_0").checked = false;
+document.getElementById("js-validContent_0").checked = false;
+document.getElementById("js-uponRequest_0").checked = false; 
+
+
+// document.getElementById("uploaderSaveButton_0").click()
+
+// 波多野 結衣
+// 涼森れむ
+// 喘ぎ 声
+// argentina
+// lana rhoades
+// 抜き
+// 日本人 かわいい
+// 最高 ボディ の 海外
+// 美女 無 修正
+// フランス
+// 日本人 かわいい
+// 最高 ボディ の 海外
+// 美女 無 修正
+// フランス
+// スク 水
+// valorant
+// ク
---
+++ videodata.upload.form_01.js
@@ -0,0 +1,321 @@
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_0.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_0.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_0.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_0.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_0").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_0").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_0").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_1.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_1.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_1.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_1.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_1.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_1.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_1").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_1").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_1").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_2.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_2.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_2.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_2.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_2.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_2.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_2").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_2").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_2").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_3.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_3.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_3.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_3.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_3.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_3.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_3").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_3").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_3").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_4.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_4.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_4.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_4.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_4.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_4.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_4").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_4").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_4").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_5.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_5.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_5.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_5.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_5.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_5.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_5").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_5").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_5").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_6.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_6.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_6.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_6.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_6.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_6.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_6").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_6").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_6").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_7.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_7.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_7.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_7.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_7.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_7.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_7").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_7").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_7").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_8.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_8.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_8.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_8.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_8.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_8.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_8").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_8").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_8").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_9.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_9.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_9.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_9.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_9.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_9.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_9").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_9").textContent="Japanese"
+
+//Tags
+document.getElementById("tagsList_9").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
+
+
+// Orientation
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_0_10.textContent="Lesbian";
+
+// Verify User
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_1_10.textContent="Yes";
+
+// Choose a Production
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_2_10.textContent="Professional";
+
+// Privacy Settings
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_10.textContent="Public";
+
+// Language Spoken in the video
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_10.textContent="Japanese";
+
+//  When will this be published
+document.getElementsByClassName("label dropDownSetTitle").dropDownTitle_3_10.textContent="Publish Now";
+
+// Title
+document.getElementById("titleTmplField_10").value = "Desperate sexy housewives decide become lesbian to survive the male drought";
+
+// Original Language
+document.getElementById("dropDownTitle_4_10").textContent="Japanese";
+
+//Tags
+document.getElementById("tagsList_10").value = 
+"natsuko kayama, mikimoto nozomi,  ベロ, キス, 欲しい, 彼女, 吸って, 舐める ,動画 hymecymeyseh, vilguinvyhpyca";
